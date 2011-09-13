c67f14410871a7742c05c3ac2a953e700463aae8
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-09-13 00:42:00 +0000

    Creé una clase File que implementa un manejador de archivos usando el File * de C, tambien incorpora un metodo para truncar la longitud del archivo. Hice un par de pruebas que dieron OK.

---
+++ estructuras/FileTest.cpp
@@ -0,0 +1,55 @@
+/*
+ * FileTest.cpp
+ *
+ *  Created on: 12/09/2011
+ *      Author: fernando
+ */
+#include "FileTest.h"
+
+using namespace std;
+
+bool FileTest::constructorTest(){
+	File f1;
+	if(f1.isOpen())
+		return false;
+	File f2("TestFile.txt",File::NEW|File::IO);
+	if(!f2.isOpen())
+		return false;
+	return true;
+}
+
+bool FileTest::writeTest1(){
+	File file("writeTest1.txt",File::NEW|File::IO);
+	char buff[12]={'h','e','l','l','o',' ','w','o','r','l','d','!'};
+	file.write(&buff,12);
+	return true;
+
+}
+bool FileTest::ioTest1(){
+	File file("ioTest1.bin",File::BIN|File::NEW|File::IO);
+	char buff[5]={0,1,2,3,4};
+		file.write(&buff,5);
+		file.seek(0,File::BEG);
+	char buff2[5];
+		file.read(&buff2,5);
+	for(int i=0 ;i<5;i++)
+		if(buff[i]!=buff2[i])
+			return false;
+	return true;
+
+}
+bool FileTest::ioTest2(){
+	File file("ioTest2.bin",File::BIN|File::NEW|File::IO);
+	char * buff= new char[100000];
+	for(int i=0;i<100000;i++)
+		buff[i]=i;
+	file.write(buff,100000);
+	file.seek(-80000,File::CUR);
+	char * buff2=new char[50000];
+	file.read(buff2,50000);
+	for(int i=0;i<50000;i++)
+		if(buff2[i]!=buff[i+20000])
+			return false;
+	return true;
+
+}
---
+++ estructuras/FileTest.h
@@ -0,0 +1,21 @@
+/*
+ * FileTest.h
+ *
+ *  Created on: 12/09/2011
+ *      Author: fernando
+ */
+
+#ifndef FILETEST_H_
+#define FILETEST_H_
+#include "File.h"
+
+class FileTest{
+
+public:
+	bool constructorTest();
+	bool writeTest1();
+	bool ioTest1();
+	bool ioTest2();
+};
+
+#endif /* FILETEST_H_ */
