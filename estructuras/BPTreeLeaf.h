8c02b939dce2f4af92e92c6d03cbaa1f0c7482f8
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-09-16 01:06:33 +0000

    La definicion de los metodos debe estar en el archivo de cabecera. Agregue la implementacion al header.

--- estructuras/BPTreeLeaf.h
+++ estructuras/BPTreeLeaf.h
@@ -9,6 +9,8 @@
 #include "stdint.h"
 #include <list>
 
+
+
 template<class Record,unsigned int blockSize>
 class BPTreeLeaf: public BPTreeNode<Record,blockSize>{
 protected:
@@ -18,13 +20,58 @@ protected:
 	typename std::list<Record>::iterator search (const Record &);
 public:
 	BPTreeLeaf(File & file);
-	BPTreeLeaf(File & file,unsigned int pos);
+	BPTreeLeaf(File & file,unsigned long pos);
 
 	BPTreeLeaf(unsigned int capacity,File & file);
-	BPTreeLeaf(unsigned int capacity,File & file,unsigned int pos);
+	BPTreeLeaf(unsigned int capacity,File & file,unsigned long pos);
 
-	virtual BPTreeLeaf * nextLeaf()=0;
+	virtual BPTreeLeaf<Record,blockSize> * nextLeaf()=0;
 	bool isLeaf()const;
 	~BPTreeLeaf();
 
 };
+
+
+
+template<class Record,unsigned int blockSize>
+BPTreeLeaf<Record,blockSize>::BPTreeLeaf(File & file):
+BPTreeNode<Record,blockSize>(file),next_(0){
+	BPTreeNode<Record,blockSize>::level_=0;
+}
+
+template<class Record,unsigned int blockSize>
+BPTreeLeaf<Record,blockSize>::BPTreeLeaf(File & file,unsigned long pos):
+BPTreeNode<Record,blockSize>(file,pos),next_(0){
+	BPTreeNode<Record,blockSize>::level_=0;
+}
+
+template<class Record,unsigned int blockSize>
+BPTreeLeaf<Record,blockSize>::BPTreeLeaf(unsigned int capacity,File & file):
+BPTreeNode<Record,blockSize>(file),next_(0),capacity_(capacity){
+	BPTreeNode<Record,blockSize>::level_=0;
+}
+
+template<class Record,unsigned int blockSize>
+BPTreeLeaf<Record,blockSize>::BPTreeLeaf(unsigned int capacity,File & file,unsigned long pos):
+BPTreeNode<Record,blockSize>(file,pos),next_(0),capacity_(capacity){
+	BPTreeNode<Record,blockSize>::level_=0;
+}
+
+
+template<class Record,unsigned int blockSize>
+typename std::list<Record>::iterator BPTreeLeaf<Record,blockSize>::search (const Record & rec){
+	typename std::list<Record>::iterator it;
+	for(it=records_.begin();it<records_.end();it++){
+       if(rec <= (**it))
+	       break;
+	}
+	return it;
+}
+
+template<class Record,unsigned int blockSize>
+bool BPTreeLeaf<Record,blockSize>::isLeaf()const{
+	return true;
+}
+
+template<class Record,unsigned int blockSize>
+BPTreeLeaf<Record,blockSize>::~BPTreeLeaf(){}
--- estructuras/BPTreeVariableLeaf.h
+++ estructuras/BPTreeVariableLeaf.h
@@ -4,6 +4,8 @@
  *  Created on: 14/09/2011
  *      Author: fernando
  */
+#ifndef BPTREEVARIABLELEAF_H_INCLUDED
+#define BPTREEVARIABLELEAF_H_INCLUDED
 
 #include "BPTreeLeaf.h"
 #include "BPTreeVariableLeafBlock.h"
@@ -14,11 +16,87 @@ class BPTreeVariableLeaf: public BPTreeLeaf<Record,blockSize>{
 	void readRecords(BPTreeVariableLeafBlock<blockSize> *);
 public:
 	BPTreeVariableLeaf(unsigned int capacity,File & file);
-	BPTreeVariableLeaf(unsigned int capacity,File & file,unsigned int pos);
+	BPTreeVariableLeaf(unsigned int capacity,File & file,unsigned long pos);
 	void read();
 	void write();
 	void insert(Record &);
 	void remove(Record &);
+	BPTreeLeaf<Record,blockSize> * nextLeaf();
 	~BPTreeVariableLeaf();
 
 };
+
+template<class Record,unsigned int blockSize>
+BPTreeVariableLeaf<Record,blockSize>::BPTreeVariableLeaf(unsigned int capacity,File & file):
+BPTreeLeaf<Record,blockSize>(capacity,file){
+	write();
+}
+
+template<class Record,unsigned int blockSize>
+BPTreeVariableLeaf<Record,blockSize>::BPTreeVariableLeaf(unsigned int capacity,File & file,unsigned long pos):
+BPTreeLeaf<Record,blockSize>(capacity,file,pos){
+	read();
+}
+
+template<class Record,unsigned int blockSize>
+void BPTreeVariableLeaf<Record,blockSize>::read(){
+	BPTreeVariableLeafBlock<blockSize> * block= new BPTreeVariableLeafBlock<blockSize>;
+
+	File & file=BPTreeNode<Record,blockSize>::file_;
+	unsigned long pos=BPTreeNode<Record,blockSize>::pos_;
+	unsigned int count=BPTreeNode<Record,blockSize>::count_;
+
+	file.seek(pos,File::BEG);
+	file.read((char*)block,blockSize);
+	count=0;
+	while(block->nestedBlocks!=0){
+		count+=block->count;
+		blockNumbers.push_back(block->next);
+		readRecords(block);
+		unsigned long nextPos=((unsigned long)block->next)*blockSize;
+		file.seek(nextPos,File::BEG);
+		file.read((char*)block,blockSize);
+	}
+	count+=block->count;
+	BPTreeNode<Record,blockSize>::count_=count;
+	BPTreeNode<Record,blockSize>::freeSpace_=block->freeSpace;
+	BPTreeLeaf<Record,blockSize>::next_=block->next;
+	readRecords(block);
+}
+
+template<class Record,unsigned int blockSize>
+void BPTreeVariableLeaf<Record,blockSize>::readRecords(BPTreeVariableLeafBlock<blockSize> * block){
+	unsigned int alreadyRead=0;
+	char * currentPos=block->bytes;
+	while(alreadyRead==block->count){
+		Record * newRecord=new Record(& currentPos);
+		BPTreeLeaf<Record,blockSize>::records_.push_back(*newRecord);
+		delete newRecord;
+		alreadyRead++;
+	}
+
+}
+
+template<class Record,unsigned int blockSize>
+void BPTreeVariableLeaf<Record,blockSize>::write(){
+	// No Implementado
+}
+
+template<class Record,unsigned int blockSize>
+void BPTreeVariableLeaf<Record,blockSize>::insert(Record &){
+	// No Implementado
+}
+
+template<class Record,unsigned int blockSize>
+void BPTreeVariableLeaf<Record,blockSize>::remove(Record &){
+	// No Implementado
+}
+template<class Record,unsigned int blockSize>
+BPTreeLeaf<Record,blockSize>* BPTreeVariableLeaf<Record,blockSize>::nextLeaf( ){
+	// No Implementado
+}
+template<class Record,unsigned int blockSize>
+BPTreeVariableLeaf<Record,blockSize>::~BPTreeVariableLeaf(){
+}
+
+#endif //BPTREEVARIABLELEAF_H
