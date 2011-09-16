ab542f16f826870b847740641fac3ff4e426bcba
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-09-16 18:52:13 +0000

    Cree la clase para probar la VariableLeaf el metodo testRead1 paso OK!(despues de mucho debugging).

---
+++ trunk/estructuras/BPTreeVariableLeafTest.h
@@ -0,0 +1,20 @@
+/*
+ * BPTreeVariableLeafTest.h
+ *
+ *  Created on: 15/09/2011
+ *      Author: fernando
+ */
+
+#ifndef BPTREEVARIABLELEAFTEST_H_
+#define BPTREEVARIABLELEAFTEST_H_
+
+#include "BPTreeVariableLeaf.h"
+
+class BPTreeVariableLeafTest {
+public:
+	BPTreeVariableLeafTest();
+	bool testRead1();
+	virtual ~BPTreeVariableLeafTest();
+};
+
+#endif /* BPTREEVARIABLELEAFTEST_H_ */
