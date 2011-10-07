998f20f41fbdc5425793caf8770565d5912343a8
juanmanuel.romeraferrio@gmail.com <juanmanuel.romeraferrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-07 03:10:35 +0000

    Agrego nuevo archivo // se supone que es en unico archvio con organizacion indexada

---
+++ managers/VoteCountingFile.cpp
@@ -0,0 +1,59 @@
+/*
+ * VoteCountingFile.cpp
+ *
+ *  Created on: 21/09/2011
+ *      Author: juanmromera
+ */
+
+#include "VoteCountingFile.h"
+
+
+VoteCountingFile::VoteCountingFile()
+{
+}
+
+
+
+VoteCountingFile::~VoteCountingFile()
+{
+}
+
+
+
+void VoteCountingFile::insert(const ElectionRecord & record)
+{
+}
+
+
+
+void VoteCountingFile::remove(const ElectionRecord & record)
+{
+}
+
+
+
+std::list<ElectionRecord> VoteCountingFile::searchByDate(uint32_t date)
+{
+}
+
+
+
+std::list<ElectionRecord> VoteCountingFile::searchByCharge(std::string charge)
+{
+}
+
+
+
+std::list<ElectionRecord> VoteCountingFile::searchByDistrict(std::string district)
+{
+}
+
+
+
+//ElectionRecord VoteCountingFile::searchByPrimaryID(uint16_t primaryId)
+//{
+//	return new ElectionRecord();
+//}
+
+
+
