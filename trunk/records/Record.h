0abd4258b1c493dc749d8271cbd1bde0c2aac3b5
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-14 15:20:51 +0000

    Los movi a la carpeta records cambien los includes.

---
+++ trunk/records/Record.h
@@ -0,0 +1,45 @@
+#ifndef RECORD_H_INCLUDED
+#define RECORD_H_INCLUDED
+
+#include <fstream>
+
+class Record{
+public:
+    class Key{
+    public:
+
+    virtual void read(char ** input)=0;
+    virtual void write(char ** output)const=0;
+    virtual unsigned int size()const=0;
+    virtual bool operator <(const Key &r)const=0;
+    virtual bool operator <=(const Key &r)const=0;
+    virtual bool operator ==(const Key &r)const=0;
+    virtual bool operator >(const Record::Key & r)const=0;
+    virtual bool operator >=(const Record::Key & r)const=0;
+    virtual bool operator !=(const Record::Key & r)const=0;
+    virtual ~Key(){}
+
+    };
+protected:
+    Key *key_;
+
+public:
+    Record(){}
+    Record(Key * kp):key_(kp){}
+    //virtual const Key & getKey()const=0;
+    //virtual void setKey(const Key & k)=0;
+    virtual void read(char ** input)=0;
+    virtual void write(char ** output)=0;
+    virtual unsigned int size()const=0;
+
+
+    bool operator <(const Record &r)const{return (*key_)<(*(r.key_));}
+    bool operator ==(const Record &r)const{return (*key_)==(*(r.key_));}
+    bool operator !=(const Record &r)const{return !((*this)==r);}
+    bool operator <=(const Record & r)const{return (*this)<r||(*this)==r;}
+    bool operator >(const Record& r)const{return !((*this)<=r);}
+    bool operator >=(const Record & r)const{return !((*this)<r);}
+    virtual ~Record(){ delete key_;}
+};
+
+#endif // RECORD_H_INCLUDED
