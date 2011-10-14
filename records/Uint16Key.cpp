fb54a9468979d9d01c68205d794a95f410574911
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-14 15:20:51 +0000

    Los movi a la carpeta records cambien los includes.

---
+++ records/Uint16Key.cpp
@@ -0,0 +1,70 @@
+#include "Uint16Key.h"
+#include <cstring>
+
+using namespace std;
+
+Uint16Key::Uint16Key(char ** input){
+    read(input);
+}
+Uint16Key::Uint16Key(uint16_t key):buffer(key){
+}
+Uint16Key::Uint16Key(const Uint16Key & uk):buffer(uk.buffer){
+}
+uint16_t Uint16Key::getKey()const{
+    return buffer;
+}
+
+void Uint16Key::setKey(uint16_t i){
+    buffer=i;
+}
+void Uint16Key::setKey(const Record::Key & rk){
+    const Uint16Key & ik= dynamic_cast<const Uint16Key &>(rk);
+    buffer=ik.buffer;
+
+}
+void Uint16Key::read(char ** input){
+    memcpy(&buffer,*input,2);
+    (*input)+=2;
+}
+void Uint16Key::write(char ** output)const{
+    memcpy(*output,&buffer,2);
+    (*output)+=2;
+}
+bool Uint16Key::operator <(const Record::Key &rk)const{
+    const Uint16Key & ik= dynamic_cast<const Uint16Key &>(rk);
+    return buffer<ik.buffer;
+}
+bool Uint16Key::operator ==(const Record::Key &rk)const{
+    const Uint16Key & ik= dynamic_cast<const Uint16Key &>(rk);
+    return buffer==ik.buffer;
+}
+bool Uint16Key::operator <=(const Record::Key &rk)const{
+    const Uint16Key & ik= dynamic_cast<const Uint16Key &>(rk);
+    return buffer<=ik.buffer;
+}
+bool Uint16Key::operator >(const Record::Key & rk)const{
+	const Uint16Key & ik= dynamic_cast<const Uint16Key &>(rk);
+    return buffer>ik.buffer;
+}
+bool Uint16Key::operator >=(const Record::Key & rk)const{
+	const Uint16Key & ik= dynamic_cast<const Uint16Key &>(rk);
+	return buffer>=ik.buffer;
+}
+bool Uint16Key::operator !=(const Record::Key & rk)const{
+	const Uint16Key & ik= dynamic_cast<const Uint16Key &>(rk);
+	return buffer!=ik.buffer;
+}
+
+Record::Key & Uint16Key::operator=(const Record::Key & rk){
+    const Uint16Key & ik= dynamic_cast<const Uint16Key &>(rk);
+    buffer=ik.buffer;
+    return (*this);
+}
+Uint16Key & Uint16Key::operator=(const Uint16Key & u){
+    buffer=u.buffer;
+    return (*this);
+}
+Uint16Key & Uint16Key::operator=(uint16_t i){
+    buffer=i;
+    return (*this);
+}
