74286ac49c6d93898733707e8367ddc2e46ea63e
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-14 16:27:44 +0000

    agregue constructor por copia y saque el fixedSize de StudentRecord

--- records/StudentRecord.h
+++ records/StudentRecord.h
@@ -10,12 +10,12 @@
 class StudentRecord : public Record{
 public:
     static const bool isVariable=true;
-     //It's variable, so fixed size must be a number > 0.
-    static const unsigned int fixedSize=1;
+
     class Key: public Uint16Key{
     public:
         Key(char ** input):Uint16Key(input){}
         Key(uint16_t key=0):Uint16Key(key){}
+        Key(const Key & key):Uint16Key(key){}
         ~Key(){}
     };
 private:
