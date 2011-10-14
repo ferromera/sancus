3cbb36ca5619061d2f552d8f03c83d0a95c2c75b
pinus06 <pinus06@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-14 03:13:03 +0000

    Arreglo los errores con los punteros a funciones , lo paso a clases.

---
+++ trunk/estructuras/Function.h
@@ -0,0 +1,50 @@
+/*
+ * Function.h
+ *
+ *  Created on: Oct 13, 2011
+ *      Author: alfredo
+ */
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+template<class T>
+class Function {
+private:
+	unsigned int size;
+public:
+	Function(unsigned int size) {
+		this->size = size;
+	}
+
+	unsigned int getSize(){
+		return size;
+	}
+
+	virtual unsigned int hash(const typename T::Key & key) = 0;
+};
+
+template<class T>
+class HashFunction: public Function<T> {
+public:
+	HashFunction(unsigned int size) : Function<T>(size) {
+	}
+
+	unsigned int hash(const typename T::Key & key) {
+		return key.getKey() % this->getSize();
+	}
+};
+
+template<class T>
+class ReHashFunction: public Function<T> {
+public:
+	ReHashFunction(unsigned int size) :
+			Function<T>(size) {
+	}
+
+	unsigned int hash(const typename T::Key & key) {
+		return (key.getKey() + 1) % this->getSize();
+	}
+};
+
+#endif /* FUNCTION_H_ */
