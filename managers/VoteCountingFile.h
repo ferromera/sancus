13589cb718d0bba19048228da96f373f85714a6a
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-16 00:26:36 +0000

    Agrego el file de conteo.

--- managers/VoteCountingFile.h
+++ managers/VoteCountingFile.h
@@ -10,30 +10,49 @@
 
 #include <list>
 #include <string>
-#include "../estructuras/VoteCountingRecord.h"
+#include "../records/VoteCountingRecord.h"
+#include "CreationMode.h"
 
-#define PATH_ELECTION_INDEX_FILE ".\ElectionIndex"
-#define PATH_DISTRICT_INDEX_FILE ".\DistrictIndex"
-#define PATH_POLITICAL_LIST_INDEX_FILE ".\PoliticalListIndex"
-#define PATH_PRIMARY_INDEX	".\VoteCountingPrimaryIndex"
-#define PATH_VOTE_COUNTNG_DATA_FILE	".\VoteCountingData"
+#define VOTE_COUNTING_ELECTION_INDEX_PATH "./VoteCountingElectionIndex.bin"
+#define VOTE_COUNTING_DISTRICT_INDEX_PATH "./VoteCountingDistrictIndex.bin"
+#define VOTE_COUNTING_LIST_INDEX_PATH "./VoteCountingListIndex.bin"
+#define VOTE_COUNTING_PRIMARY_INDEX_PATH	"./VoteCountingPrimaryIndex.bin"
+#define VOTE_COUNTING_DATA_PATH	"./VoteCountingData.bin"
+
+
 
 class VoteCountingFile {
+	enum lastSearchEnum {ELECTION_SEARCH,DISTRICT_SEARCH,LIST_SEARCH,PRIMARY_SEARCH,NO_SEARCH};
+
+	BPlusTree<SecondaryIndexRecord<ElectionRecord::Key,VoteCountingRecord::Key>,4096> * electionIndex;
+	BPlusTree<SecondaryIndexRecord<DistrictRecord::Key,VoteCountingRecord::Key>,4096> * districtIndex;
+	BPlusTree<SecondaryIndexRecord<ListRecord::Key,VoteCountingRecord::Key>,4096> * listIndex;
+	BPlusTree<PrimaryIndexRecord<VoteCountingRecord::Key>,4096> * primaryIndex;
+	IndexedDataFile<VoteCountingRecord,8192> * dataFile;
+	enum lastSearchEnum lastSearch;
+
+
+
+	static VoteCountingFile * instance;
+	static VoteCountingFile * getInstance();
+
 public:
-	VoteCountingFile();
-	virtual ~VoteCountingFile();
+	VoteCountingFile(int creationMode=CREATE);
+
 	void insert(const VoteCountingRecord &);
 	void remove(const VoteCountingRecord &);
-	std::list<VoteCountingRecord> searchByElection(uint32_t);
-	std::list<VoteCountingRecord> searchByPoliticalList(std::string);
-	std::list<VoteCountingRecord> searchByDistrict(std::string);
-	//ElectionRecord searchByPrimaryID(uint16_t);
+	void update(const VoteCountingRecord &);
+	const VoteCountingRecord & searchByDistrict(const DistrictRecord::Key & district);
+	const VoteCountingRecord & searchByElection(const ElectionRecord::Key & district);
+	const VoteCountingRecord & searchByList(const ListRecord::Key & district);
+	const VoteCountingRecord & search(const VoteCountingRecord::Key & district);
+	const VoteCountingRecord & VoteCountingFile::nextDistrict();
+	const VoteCountingRecord & VoteCountingFile::nextElection();
+	const VoteCountingRecord & VoteCountingFile::nextList();
+	const VoteCountingRecord & VoteCountingFile::next();
 
-private:
-	//BPlusTree<ElectionIndexRecord,512> *primaryIndex;
-	//BPlusTree<DateIndexRecord,512>     *dateIndex;
-	//BPlusTree<ChargeIndexRecord,512>   *chargeIndex;
-	//BPlusTree<DistrictIndexRecord,512> *districIndex;
+	virtual ~VoteCountingFile();
+
 };
 
 #endif /* VOTECOUNTINGFILE_h */
