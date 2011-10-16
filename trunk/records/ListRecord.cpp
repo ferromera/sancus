5a58bdf322ef93ffa0e7c9a9b88d6f99d5ca006c
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-16 00:15:00 +0000

    Modifique el ListRecord , no se si esta OK.

---
+++ trunk/records/ListRecord.cpp
@@ -0,0 +1,48 @@
+/*
+ * ListRecord.cpp
+ *
+ *  Created on: 15/10/2011
+ *      Author: fernando
+ */
+
+#include "ListRecord.cpp"
+
+ListRecord::ListRecord(const ListRecord::Key & k){
+	key_= new ListRecord::Key(k);
+}
+ListRecord::ListRecord(const ElectionRecord::Key& election,const std::string & listName){
+	key_= new ListRecord::Key(election,listName);
+}
+unsigned int ListRecord::size()const{
+	return key_->size();
+}
+void ListRecord::setKey(const ListRecord::Key & k){
+	delete key_;
+	key_= new ListRecord::Key(k);
+}
+void ListRecord::setKey(const std::string & listName, ElectionRecord::Key* election){
+	delete key_;
+	key_= new ListRecord::Key(election,listName);
+}
+void ListRecord::read(char ** input){
+	key_->read(input);
+}
+void ListRecord::write(char ** output){
+	key_->write(output);
+}
+void ListRecord::setName(const std::string & listName){
+	((Key*)key_)->setName(listName);
+}
+const std::string & ListRecord::getName()const{
+	return ((Key*)key_)->getName();
+}
+const ListRecord::Key & ListRecord::getKey()const{
+	return *((Key*)key_);
+}
+void ListRecord::setElection (const ElectionRecord::Key & election){
+	((Key*)key_)->setElection(election);
+}
+const ElectionRecord::Key & ListRecord::getElection()const{
+	return ((Key*)key_)->getElection();
+}
+ListRecord::~ListRecord(){}
