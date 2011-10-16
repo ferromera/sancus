0d69f30e4b6e43af11e4e6d5d7df6f957033793b
pinus06 <pinus06@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-16 05:08:57 +0000

    Arreglo varias cositas

--- trunk/estructuras/HashTableTest.h
+++ trunk/estructuras/HashTableTest.h
@@ -20,15 +20,18 @@ private:
 
 public:
 
-	void assertTrue(bool condition, string message) {
+	void assertTrue(bool condition,string testname, string message) {
 		if (!condition) {
+			cout << testname << " FAIL."<<endl;
 			cout << message << endl;
 			throw new exception();
+		}else{
+			cout << testname << " OK."<<endl;
 		}
 	}
 
-	void assertFalse(bool condition, string message) {
-		assertTrue(!condition, message);
+	void assertFalse(bool condition,string testname, string message) {
+		assertTrue(!condition,testname, message);
 	}
 
 	void testCreate() {
@@ -36,7 +39,7 @@ public:
 
 		table = new HashTable<StudentRecord, 512>(path, 10, 100);
 
-		assertTrue(MathUtils::isPrime(table->getSize()),"El tamaño de la tabla debe ser primo");
+		assertTrue(MathUtils::isPrime(table->getSize()),"testCreate","El tamaño de la tabla debe ser primo");
 
 	}
 
@@ -49,7 +52,7 @@ public:
 
 		StudentRecord * record2 = table->get(record->getKey());
 
-		assertTrue(record->getKey() == record2->getKey(),"Los records no son iguales");
+		assertTrue((*record) == (*record2),"testInsertAndGet","Los records no son iguales");
 	}
 
 	void testUniquenessValidation() {
@@ -67,6 +70,33 @@ public:
 		StudentRecord::Key * key = new StudentRecord::Key(10);
 
 		table->get(*key);
+	}
+
+	void testRemoveRecord(){
+		testCreate();
+
+		bool removed = false;
+
+		StudentRecord * record = new StudentRecord(12, "alfredo");
+		StudentRecord * record2;
+
+		table->insert(*record);
+
+		table->remove(*record);
+
+		try{
+			record2 = table->get(record->getKey());
+		}catch (RecordNotFoundException & ex){
+			removed = true;
+			assertTrue(record2 == NULL, "testRemoveRecord", "El record no es null");
+			cout<<"RecordNotFoundException lanzada."<<endl;
+		}
+
+		if(!removed){
+			cout<<"testRemoveRecord FAIL"<<endl;
+		}
+
+
 	}
 
 };
