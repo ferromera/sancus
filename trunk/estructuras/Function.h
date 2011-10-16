40ed597025e2575b98c8ca8319c54256aa53e7a9
pinus06 <pinus06@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-16 17:15:27 +0000

    Agrego las validaciones para saber si la clave es string o int

--- trunk/estructuras/Function.h
+++ trunk/estructuras/Function.h
@@ -12,38 +12,52 @@ template<class T>
 class Function {
 private:
 	unsigned int size;
+
+	unsigned int convertToInt(const std::string & key) {
+		return 0;
+	}
+
+	virtual unsigned int
+			hashTemplateMethod(unsigned int key, unsigned int jump) = 0;
 public:
 	Function(unsigned int size) {
 		this->size = size;
 	}
 
-	unsigned int getSize(){
+	unsigned int getSize() {
 		return size;
 	}
 
-	virtual unsigned int hash(const typename T::Key & key, unsigned int jump) = 0;
+	unsigned int hash(const typename T::Key & key, unsigned int jump) {
+		if (T::Key::isString)
+			return hashTemplateMethod(convertToInt(key.getString()), jump);
+		else
+			return hashTemplateMethod(key.getUint(), jump);
+	}
 };
 
 template<class T>
 class HashFunction: public Function<T> {
-public:
-	HashFunction(unsigned int size) : Function<T>(size) {
+private:
+	unsigned int hashTemplateMethod(unsigned int key, unsigned int jump) {
+		return key % this->getSize();
 	}
-
-	unsigned int hash(const typename T::Key & key, unsigned int jump) {
-		return key.getKey() % this->getSize();
+public:
+	HashFunction(unsigned int size) :
+		Function<T> (size) {
 	}
 };
 
 template<class T>
 class ReHashFunction: public Function<T> {
-public:
-	ReHashFunction(unsigned int size) :
-			Function<T>(size) {
+private:
+	unsigned int hashTemplateMethod(unsigned int key, unsigned int jump) {
+		return (jump + (key % 5) + 1) % this->getSize();
 	}
 
-	unsigned int hash(const typename T::Key & key, unsigned int jump) {
-		return (jump + (key.getKey() % 5) + 1) % this->getSize();
+public:
+	ReHashFunction(unsigned int size) :
+		Function<T> (size) {
 	}
 };
 
