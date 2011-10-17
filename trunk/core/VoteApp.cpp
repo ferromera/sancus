fc773f2da255db3eebedce2c364b7d00938a8f98
juanmanuel.romeraferrio@gmail.com <juanmanuel.romeraferrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-17 01:28:40 +0000

    subo lo que sera la aplicacion

---
+++ trunk/core/VoteApp.cpp
@@ -0,0 +1,84 @@
+/*
+ * VoteApp.cpp
+ *
+ *  Created on: 14/10/2011
+ *      Author: juanmromera
+ */
+
+#include "VoteApp.h"
+#include "../screens/Drawable.h"
+#include "../screens/LoginScreen.h"
+#include "../screens/ElectionScreen.h"
+#include "../screens/ListScreen.h"
+#include "../screens/AdministratorScreen.h"
+#include "../screens/ChargeScreen.h"
+
+VoteApp* VoteApp::voteApp = NULL;
+
+VoteApp::VoteApp() {
+
+	Drawable *scr1 = new LoginScreen();
+	Drawable *scr2 = new ElectionScreen();
+	Drawable *scr3 = new ListScreen();
+	Drawable *scr4 = new AdministratorScreen();
+	Drawable *scr5 = new ChargeScreen();
+
+	this->screens.push_back(scr1);
+	this->screens.push_back(scr2);
+	this->screens.push_back(scr3);
+	this->screens.push_back(scr4);
+	this->screens.push_back(scr5);
+
+	actualScreen = ADM_SCREEN;
+
+}
+VoteApp* VoteApp::getInstance()
+{
+	if ( VoteApp::voteApp  == NULL )
+		VoteApp::voteApp = new VoteApp();
+	return VoteApp::voteApp;
+}
+
+void VoteApp::run(){
+	while(true)
+	{
+		std::list<Drawable*>::iterator screen  = getToScreen(actualScreen);
+		(*screen)->draw();
+	}
+}
+std::list<Drawable*>::iterator VoteApp::getToScreen(unsigned int screenNumber)
+{
+	std::list<Drawable*>::iterator itList = screens.begin();
+	int a = screens.size();
+	int i = 0;
+	while(i < screenNumber)
+	{
+		itList++;
+		i++;
+	}
+
+
+	return itList;
+}
+void VoteApp::setActualScreen(unsigned int number){
+	actualScreen = number;
+}
+/*
+void VoteApp::setUserLogin(VoterRecord user)
+{
+	userLogin = user;
+}
+void VoteApp::setChooseElection(ElectionRecord election)
+{
+	chooseElection = election;
+}
+VoterRecord VoteApp::getUserLogin()
+{
+	return userLogin;
+}
+ElectionRecord VoteApp::getChooseElection()
+{
+	return chooseElection;
+}
+*/
+
---
+++ trunk/core/VoteApp.h
@@ -0,0 +1,51 @@
+/*
+ * VoteApp.h
+ *
+ *  Created on: 14/10/2011
+ *      Author: juanmromera
+ */
+
+#ifndef VOTEAPP_H_
+#define VOTEAPP_H_
+
+#include <list>
+#include "Runable.h"
+#include "stdint.h"
+//#include "../records/VoterRecord.h"
+//#include "../records/ElectionRecord.h"
+#include "../screens/Drawable.h"
+#define LOGIN_SCREEN 0
+#define ELECTION_SCREEN 1
+#define LIST_SCREEN 2
+#define ADM_SCREEN 3
+#define ALTA_SCREEN 4
+#define BAJA_SCREEN 5
+#define MOD_SCREEN 6
+#define CONSULTA_SCREEN 7
+
+class VoteApp : public Runable{
+public:
+	VoteApp();
+	void run();
+	void setActualScreen(unsigned int);
+	uint32_t dni;
+	static VoteApp* getInstance();
+	/*
+	void setUserLogin(VoterRecord);
+	void setChooseElection(ElectionRecord);
+	VoterRecord getUserLogin();
+	ElectionRecord getChooseElection();
+	*/
+private:
+	std::list<Drawable*> screens;
+	std::list<Drawable*>::iterator getToScreen(unsigned int);
+	unsigned int actualScreen;
+	static VoteApp* voteApp;
+	/*
+	VoterRecord userLogin;
+	ElectionRecord chooseElection;
+	*/
+
+};
+
+#endif /* VOTEAPP_H_ */
