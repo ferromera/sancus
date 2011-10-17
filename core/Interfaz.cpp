44565d0323f6a8f4f57a220ea11be54f1ca7e4fd
juanmanuel.romeraferrio@gmail.com <juanmanuel.romeraferrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-17 01:28:40 +0000

    subo lo que sera la aplicacion

---
+++ core/Interfaz.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : Interfaz.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "VoteApp.h"
+using namespace std;
+
+int main() {
+	//cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	VoteApp *app = VoteApp::getInstance();
+	app->run();
+	delete app;
+	return 0;
+}
