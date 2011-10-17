b6d2c47db13b513d3541aaa5f790b26b0494b259
FernandoRomeraFerrio@gmail.com <FernandoRomeraFerrio@gmail.com@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-17 18:25:27 +0000

    Arregle el load del arbol

--- estructuras/BPTreeVariableLeafBlock.h
+++ estructuras/BPTreeVariableLeafBlock.h
@@ -10,13 +10,14 @@
 
 #include "stdint.h"
 
-#define VARIABLE_LEAF_CONTROL_BYTES 6
+#define VARIABLE_LEAF_CONTROL_BYTES 7
 
 #pragma pack(push)
 #pragma pack(1)
 template<unsigned int blockSize>
 struct BPTreeVariableLeafBlock{
 	uint16_t freeSpace;
+	uint8_t level;
 	uint32_t next; //siguiente bloque anidado o siguiente hoja.
 	char bytes[blockSize - VARIABLE_LEAF_CONTROL_BYTES];
 };
