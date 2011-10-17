ddb2d8a55ae37f62792e3f488888180a1e1304f5
ceciliakhalil <ceciliakhalil@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-17 21:58:52 +0000

    subo VoterFile

---
+++ trunk/managers/VoterFile.h
@@ -0,0 +1,36 @@
+/*
+ * VoterFile.h
+ *
+ *  Created on: 09/10/2011
+ *      Author: cecilia
+ */
+
+#ifndef VOTERFILE_H_
+#define VOTERFILE_H_
+
+#include "../records/VoterRecord.h"
+#include "../estructuras/HashTable.h"
+#include "FileManagerExceptions.h"
+
+#define VOTER_FILE_DATA_PATH	(std::string &) "./DistrictFileData.bin"
+#define VOTER_FILE_RECORDS_PER_BUCKET 50
+#define VOTER_FILE_MAX_NUMBER_OF_RECORDS 10000
+
+class VoterFile {
+private:
+	HashTable<VoterRecord,4096> * table;
+	static VoterFile * instance;
+public:
+	static VoterFile * getInstance();
+
+	VoterFile();
+
+    virtual ~VoterFile();
+	void insert(const VoterRecord &);
+	void remove(const VoterRecord &);
+	void update(const VoterRecord &);
+	const VoterRecord & search (const VoterRecord::Key &);
+};
+
+
+#endif /* VOTERFILE_H_ */
