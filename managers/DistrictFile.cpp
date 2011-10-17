d35b321d4a4198d7ee83cd597dd56b6e28282520
pinus06 <pinus06@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-17 01:02:41 +0000

    Agrego el esqueleto de DistrictFile

---
+++ managers/DistrictFile.cpp
@@ -0,0 +1,12 @@
+/*
+ * DistrictFile.cpp
+ *
+ *  Created on: Oct 16, 2011
+ *      Author: alfredo
+ */
+
+#include "DistrictFile.h"
+
+DistrictFile::~DistrictFile(){
+	delete (table);
+}
---
+++ managers/DistrictFile.h
@@ -0,0 +1,31 @@
+/*
+ * DistrictFile.h
+ *
+ *  Created on: Oct 16, 2011
+ *      Author: alfredo
+ */
+
+#ifndef DISTRICTFILE_H_
+#define DISTRICTFILE_H_
+
+#include "../records/DistrictRecord.h"
+#include "../estructuras/HashTable.h"
+
+#define DISTRICT_FILE_DATA_PATH	"./DistrictFileData.bin"
+
+class DistrictFile {
+private:
+	HashTable<DistrictRecord,4096> * table;
+	static DistrictFile * instance;
+public:
+	static DistrictFile * getInstance();
+
+	void insert(const DistrictRecord &);
+	void remove(const DistrictRecord &);
+	void update(const DistrictRecord &);
+	const DistrictRecord & search(const DistrictRecord::Key & district);
+
+	virtual ~DistrictFile();
+};
+
+#endif /* DISTRICTFILE_H_ */
