497d8850432788bcb065853b24aaababf5ebad41
ceciliakhalil <ceciliakhalil@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-17 21:58:52 +0000

    subo VoterFile

---
+++ managers/VoterFile.cpp
@@ -0,0 +1,58 @@
+/*
+ * VoterFile.cpp
+ *
+ *  Created on: 17/10/2011
+ *      Author: cecilia
+ */
+
+#include "VoterFile.h"
+
+VoterFile* VoterFile::instance = NULL;
+
+VoterFile * VoterFile::getInstance(){
+	if(instance == NULL)
+		instance= new VoterFile();
+	return instance;
+}
+
+VoterFile::VoterFile() {
+	this->table = new HashTable<VoterRecord, 4096> (VOTER_FILE_DATA_PATH,
+				VOTER_FILE_RECORDS_PER_BUCKET, VOTER_FILE_MAX_NUMBER_OF_RECORDS);
+}
+
+
+void VoterFile::insert(const VoterRecord & vr){
+	try {
+			table->insert(vr);
+	} catch (RehashCountException & ex) {
+			throw FileInsertException();
+	}
+}
+
+void VoterFile::remove(const VoterRecord & vr) {
+	try {
+		table->remove(vr);
+	} catch (RehashCountException & ex){
+		throw FileRemoveException();
+	}
+}
+
+const VoterRecord & VoterFile::search(const VoterRecord::Key & vr) {
+	try {
+		return table->get(vr);
+	} catch (RecordNotFoundException & ex) {
+		throw FileSearchException();
+	}
+}
+
+void VoterFile::update(const VoterRecord & vr) {
+	try {
+		table->update(vr);
+	} catch (RecordNotFoundException & ex) {
+		throw FileUpdateException();
+	}
+}
+
+VoterFile::~VoterFile() {
+	delete (table);
+}
