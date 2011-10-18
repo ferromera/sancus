9b7be3c3b3cf9d2183714851ec04e5e0bf8ade4d
ceciliakhalil <ceciliakhalil@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-18 16:01:47 +0000

    aca va AdministratorFile

---
+++ managers/AdministratorFile.h
@@ -0,0 +1,35 @@
+/*
+ * AdministratorFile.h
+ *
+ *  Created on: 18/10/2011
+ *      Author: cecilia
+ */
+
+#ifndef ADMINISTRATORFILE_H_
+#define ADMINISTRATORFILE_H_
+#include "../records/AdministratorRecord.h"
+#include "../estructuras/HashTable.h"
+
+#define ADMINISTRATOR_FILE_DATA_PATH	"AdministratorFileData.bin"
+#define ADMINISTRATOR_FILE_RECORDS_PER_BUCKET 50
+#define ADMINISTRATOR_FILE_MAX_NUMBER_OF_RECORDS	10000
+
+class AdministratorFile {
+private:
+	HashTable<AdministratorRecord,4096> * table;
+	static AdministratorFile * instance;
+public:
+	static AdministratorFile * getInstance();
+
+	AdministratorFile();
+
+	void insert(const AdministratorRecord &);
+	void remove(const AdministratorRecord &);
+	void update(const AdministratorRecord &);
+	const AdministratorRecord & search(const AdministratorRecord::Key &);
+
+	virtual ~AdministratorFile();
+};
+
+
+#endif /* ADMINISTRATORFILE_H_ */
