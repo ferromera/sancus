ecab6ab8a191fbd5492064a0509bfd6441f27e34
pinus06 <pinus06@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-10-19 18:18:52 +0000

    Arreglo todos los errores de compilacion que eran muchos

--- generators/Generator.h
+++ generators/Generator.h
@@ -21,6 +21,30 @@
 
 using namespace std;
 
+#define ARGENTINA "Argentina"
+
+#define NUMERO_DE_PROVINCIAS  23
+#define NUMERO_DE_MUNICIPIOS  50
+#define NUMERO_DE_VOTANTES  10000
+#define NUMERO_DE_LISTAS_POR_ELECCION  10000
+#define DOCUMENTO_BASE  20000000
+
+#define NOMBRE_BASE_PROVINCIAS  "Provincia"
+#define  NOMBRE_BASE_MUNICIPIOS  "Municipio"
+#define  NOMBRE_BASE_LISTA "Lista"
+#define  NOMBRE_BASE_VOTANTE  "Votante"
+
+#define  PRESIDENTE  "Presidente"
+#define  VICEPRESIDENTE "Vicepresidente"
+#define  SENADOR  "Senador"
+#define  DIPUTADO  "Diputado"
+#define  GOBERNADOR  "Gobernador"
+#define  VICEGOBERNADOR  "Vicegobernador"
+#define  LEGISLADOR "Legislador"
+#define  INTENDENTE "Intendente"
+#define  VICEINTENDENTE  "ViceIntendente"
+#define  CONSEJAL "Consejal"
+
 class Generator {
 private:
 	DistrictFile * districts;
@@ -29,30 +53,6 @@ private:
 	VoteCountingFile * voteCountings;
 	CandidateFile * candidates;
 	ChargeFile * charges;
-
-	const string ARGENTINA = "Argentina";
-
-	const unsigned int NUMERO_DE_PROVINCIAS = 23;
-	const unsigned int NUMERO_DE_MUNICIPIOS = 50;
-	const unsigned int NUMERO_DE_VOTANTES = 10000;
-	const unsigned int NUMERO_DE_LISTAS_POR_ELECCION = 10000;
-	const unsigned int DOCUMENTO_BASE = 20000000;
-
-	const string NOMBRE_BASE_PROVINCIAS = "Provincia";
-	const string NOMBRE_BASE_MUNICIPIOS = "Municipio";
-	const string NOMBRE_BASE_LISTA = "Lista";
-	const string NOMBRE_BASE_VOTANTE = "Votante";
-
-	const string PRESIDENTE = "Presidente";
-	const string VICEPRESIDENTE = "Vicepresidente";
-	const string SENADOR = "Senador";
-	const string DIPUTADO = "Diputado";
-	const string GOBERNADOR = "Gobernador";
-	const string VICEGOBERNADOR = "Vicegobernador";
-	const string LEGISLADOR = "Legislador";
-	const string INTENDENTE = "Intendente";
-	const string VICEINTENDENTE = "ViceIntendente";
-	const string CONSEJAL = "Consejal";
 
 	unsigned int date;
 	unsigned int currentDNI;
@@ -93,7 +93,7 @@ private:
 
 		for (unsigned int i = 0; i < NUMERO_DE_PROVINCIAS; i++) {
 			string provincia = NOMBRE_BASE_PROVINCIAS + StringUtils::intToString(i);
-			provinciaRecord = new DistrictRecord(provincia, argentina);
+			provinciaRecord = new DistrictRecord(provincia, ARGENTINA);
 
 			districts->insert(*provinciaRecord);
 			string chargesProvinciales[] = { GOBERNADOR, VICEGOBERNADOR, LEGISLADOR };
@@ -102,8 +102,9 @@ private:
 			delete (provinciaRecord);
 
 			for (unsigned int j = 0; j < NUMERO_DE_MUNICIPIOS; j++) {
-				string municipio = NOMBRE_BASE_MUNICIPIOS + StringUtils::intToString(j) + "-"+ provincia;
-				municipioRecord = new DistrictRecord(retiro, provincia);
+				string municipio = NOMBRE_BASE_MUNICIPIOS + StringUtils::intToString(j) + "-"
+								+ provincia;
+				municipioRecord = new DistrictRecord(municipio, provincia);
 				string chargesMunicipales[] = { INTENDENTE, VICEINTENDENTE, CONSEJAL };
 
 				loadVoters(municipioRecord);
@@ -115,20 +116,21 @@ private:
 		}
 	}
 
-	void loadCharges(String charges[], unsigned int numberOfCharges, DistrictRecord * district) {
+	void loadCharges(string chargesArray[], unsigned int numberOfCharges, DistrictRecord * district) {
 
-		ChargeRecord * fatherRecord = new ChargeRecord(charges[0], district->getDistrictName());
-		chargeFile->insert(*fatherRecord);
+		ChargeRecord * fatherRecord =
+						new ChargeRecord(chargesArray[0], district->getDistrictName());
+		charges->insert(*fatherRecord);
 
 		ElectionRecord * election = loadElection(fatherRecord, district);
 
 		ChargeRecord * childRecord;
 
 		for (unsigned int i = 1; i < numberOfCharges; i++) {
-			childRecord = new ChargeRecord(charges[i], district->getDistrictName(), charges[0],
-							district->getFatherName());
+			childRecord = new ChargeRecord(chargesArray[i], district->getDistrictName(),
+							chargesArray[0], district->getFatherName());
 
-			chargeFile->insert(*childRecord);
+			charges->insert(*childRecord);
 
 			loadList(election);
 
@@ -157,7 +159,7 @@ private:
 			string list = NOMBRE_BASE_LISTA + i;
 			listRecord = new ListRecord(election->getKey(), NOMBRE_BASE_LISTA);
 
-			lists->insert(listRecord);
+			lists->insert(*listRecord);
 
 			loadVoteCountings(listRecord);
 
@@ -167,9 +169,9 @@ private:
 
 	void loadVoteCountings(ListRecord * list) {
 		VoteCountingRecord * voteCountingRecord = new VoteCountingRecord(list->getKey(),
-						list->getElection(), list->getElection().getCharge().getDistrict());
+						list->getElection().getCharge().getDistrict(), list->getElection(), 0);
 
-		voteCountings->insert(voteCountingRecord);
+		voteCountings->insert(*voteCountingRecord);
 
 		delete (voteCountingRecord);
 	}
@@ -178,8 +180,12 @@ private:
 		unsigned int dni = currentDNI - 1;
 		VoterRecord::Key * voterKey = new VoterRecord::Key(dni);
 
-		CandidateRecord * candidateRecord = new CandidateRecord(list->getKey(voterKey),
-						list->getElection().getCharge(), NULL);
+		CandidateRecord * candidateRecord = new CandidateRecord(list->getKey(),
+						list->getElection().getCharge(), voterKey->getKey());
+
+		candidates->insert(*candidateRecord);
+
+		delete (candidateRecord);
 	}
 
 	void loadVoters(DistrictRecord * district) {
@@ -189,15 +195,16 @@ private:
 			unsigned int dni = currentDNI++;
 			string voterName = NOMBRE_BASE_VOTANTE + StringUtils::intToString(dni);
 			string voterKey = "password";
-			list<ElectionRecord::Key> & elections;
+			list<ElectionRecord::Key> elections;
 
 			VoterRecord * voterRecord = new VoterRecord(voterName, dni, address, voterKey,
 							district->getKey(), elections);
 
 			voters->insert(*voterRecord);
 
-			delete (voter);
+			delete (voterRecord);
 		}
 	}
+
 };
 #endif /* GENERATOR_H_ */
