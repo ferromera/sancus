32f5dfe18a0b44e66665964b8340b0a37be6897e
pinus06 <pinus06@b06ae71c-7d8b-23a7-3f8b-8cb8ce3c93c2>
2011-11-18 04:16:56 +0000

    agrego euclides extendido y un test, y ademas agrego lo que charlamos con ceci sobre vigenere

--- trunk/estructuras/VigenereSecurity.h
+++ trunk/estructuras/VigenereSecurity.h
@@ -9,14 +9,30 @@
 #define VIGENERE_SECURITY_H_
 
 class VigenereSecurity : public SecurityStrategy {
+private:
+	char * key;
+
+	void generateAndStoreRandomKey(unsigned keySize);
 
 public:
 	VigenereSecurity(unsigned int  keySize): SecurityStrategy(keySize){
+		key = new char[keySize];
 
+		generateAndStoreRandomKey(keySize);
 	}
 
 	void encrypt(void * buffer, size_t bytes){
+		unsigned int j=0;
 
+		for(unsigned int i = 0; i< bytes; i++){
+			if(j>this->keySize){
+				j=0;
+			}
+
+			j++;
+
+			buffer[i] = (key[j] + buffer[i]  % 256);
+		}
 	}
 	
 	void decrypt(void * buffer, size_t bytes){
